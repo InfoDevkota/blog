7bf864e451a0b9d162db01a026df31497f5cb439
Sagar Devkota <devkota@bloggernepal.com>
2017-04-20 23:00:40 +0545

    creating models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class BlogPost(models.Model):
+    author = models.ForeignKey(User)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
