76b338a3ccd129564d9c144aedc56c34a4f58cce
Sagar Devkota <devkota@bloggernepal.com>
2017-04-30 10:04:34 +0545

    post and page and category

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import BlogPost
+from .models import BlogPost, BlogPage, Category
 
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ['id', 'title']
     prepopulated_fields = {'slug':('title',)}
 
-admin.site.register(BlogPost,BlogPostAdmin)
+class BlogPageAdmin(admin.ModelAdmin):
+    list_display = ['id','title']
+    prepopulated_fields = {'slug':('title',)}
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id','name']
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(BlogPost, BlogPostAdmin)
+admin.site.register(BlogPage, BlogPageAdmin)
+admin.site.register(Category, CategoryAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,32 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(max_length=20)
+
+    def __str__(self):
+        return self.name
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User)
+    date_published = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+    category = models.ForeignKey(Category)
+
+    content = models.TextField()
+
+
+    def __str__(self):
+        return self.title
+
+class BlogPage(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
     content = models.TextField()
 
     def __str__(self):
